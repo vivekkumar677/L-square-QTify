b8f1d4d11828f5871701450f61658247c069d431
Vivek Kumar <ervivekkumar677@gmail.com>
2024-05-08 05:15:43 +0530

    Versioning untracked files

--- qtify/src/components/Navbar/navbar.jsx
+++ qtify/src/components/Navbar/navbar.jsx
@@ -42,7 +42,6 @@ export default function Navbar(){
                         <SearchIcon className='searchIcon'/>
                     </div>
                     <Button/>
-                  
                 </Box>
             </Box>
         </>
